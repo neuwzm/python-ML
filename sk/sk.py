1a75b3bcb427de8bdf5843406e46749b9d8ab30c
zeming wang <qazwsxwzm@gmail.com>
2017-12-19 23:46:45 +0800

    svm

--- sk/sk.py
+++ sk/sk.py
@@ -168,4 +168,8 @@ plt.legend()
 plt.show()
 
 
-
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
