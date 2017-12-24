de9daa3a7f735bb3eff7fe0742c926c543485c32
zeming wang <qazwsxwzm@gmail.com>
2017-12-24 11:16:56 +0800

    rbf kernel

--- sk/sk.py
+++ sk/sk.py
@@ -183,3 +183,18 @@ plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.show()
+
+
+   # gamma参数值过大会导致模型过拟合
+    svm = SVC(kernel='rbf', random_state=0, gamma=100.0, C=1.0)
+    svm.fit(X_train_std, y_train)
+    plot_decision_regions(X_combined_std,
+   ...                       y_combined, classifier=svm,
+   ...                       test_idx=range(105,150))
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+
