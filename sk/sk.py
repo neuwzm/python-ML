54df4404fbff259393a787f83be00339a8c6130c
zeming wang <qazwsxwzm@gmail.com>
2017-12-26 00:32:52 +0800

    decsion tree

--- sk/sk.py
+++ sk/sk.py
@@ -8855,4 +8855,15 @@ plt.show()
 
 
 
+    from sklearn.tree import DecisionTreeClassifier
+    tree = DecisionTreeClassifier(criterion='entropy',max_depth=3, random_state=0)
+    tree.fit(X_train, y_train)
+    X_combined = np.vstack((X_train, X_test))
+    y_combined = np.hstack((y_train, y_test))
+    plot_decision_regions(X_combined, y_combined,   classifier=tree, test_idx=range(105,150))
+    plt.xlabel('petal length [cm]')
+    plt.ylabel('petal width [cm]')
+    plt.legend(loc='upper left')
+    plt.show()
+
 
